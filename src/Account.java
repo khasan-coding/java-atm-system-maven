0fa59210236fbb264f76879c03340c7e2c30e0ae
Khasan <kyuldashev2000@icloud.com>
2026-05-26 11:36:51 -0400

    Add deposit method to Account class

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,18 @@
 public class Account {
+
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double startingBalance){
+        this.accountNumber = accountNumber;
+        this.balance = startingBalance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
 }
