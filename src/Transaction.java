3bcd89de77ddf0158ed27d7fbe7ae9bf7030419d
Khasan <kyuldashev2000@icloud.com>
2026-05-26 16:18:59 -0400

    Create Transaction class for transaction history

---
+++ src/Transaction.java
@@ -0,0 +1,18 @@
+public class Transaction {
+
+    private String type;
+    private double amount;
+    private double balanceAfterTransaction;
+
+
+    public Transaction(String type, double amount, double balanceAfterTransaction) {
+        this.type = type;
+        this.amount = amount;
+        this.balanceAfterTransaction = balanceAfterTransaction;
+    }
+
+    // Displays the transaction details in a readable format
+    public void displayTransaction() {
+        System.out.println(type + ": $" + amount + " | Balance after: $" + balanceAfterTransaction);
+    }
+}
