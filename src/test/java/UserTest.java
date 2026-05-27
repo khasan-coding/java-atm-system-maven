415b106692bf66e5c3f6ddbf5c3bc3b05a6115ec
Khasan <kyuldashev2000@icloud.com>
2026-05-27 12:12:16 -0400

    Update User tests for checking and savings accounts

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,41 +7,80 @@ public class UserTest {
 
     @Test
     void correctPinShouldReturnTrue() {
-        Account account = new Account("1001", 500.00);
-        User user = new User("Khasan", "123456", "1111", account);
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
 
         assertTrue(user.isPinCorrect("1111"));
     }
 
     @Test
     void wrongPinShouldReturnFalse() {
-        Account account = new Account("1001", 500.00);
-        User user = new User("Khasan", "123456", "1111", account);
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
 
         assertFalse(user.isPinCorrect("9999"));
     }
 
     @Test
     void correctCardNumberShouldReturnTrue() {
-        Account account = new Account("1001", 500.00);
-        User user = new User("Khasan", "123456", "1111", account);
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
 
         assertTrue(user.isCardNumberCorrect("123456"));
     }
 
     @Test
     void wrongCardNumberShouldReturnFalse() {
-        Account account = new Account("1001", 500.00);
-        User user = new User("Khasan", "123456", "1111", account);
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
 
         assertFalse(user.isCardNumberCorrect("000000"));
     }
 
     @Test
-    void userShouldHaveConnectedAccount() {
-        Account account = new Account("1001", 500.00);
-        User user = new User("Khasan", "123456", "1111", account);
+    void userShouldHaveConnectedCheckingAccount() {
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
 
-        assertTrue(user.getAccount() == account);
+        assertTrue(user.getCheckingAccount() == checkingAccount);
+    }
+
+    @Test
+    void userShouldHaveConnectedSavingsAccount() {
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
+
+        assertTrue(user.getSavingsAccount() == savingsAccount);
+    }
+
+    @Test
+    void userWithCheckingAndSavingsShouldHaveBothAccounts() {
+        Account checkingAccount = new Account("1001", 500.00);
+        Account savingsAccount = new Account("1002", 1000.00);
+        User user = new User("Khasan", "123456", "1111", checkingAccount, savingsAccount);
+
+        assertTrue(user.hasBothAccounts());
+    }
+
+    @Test
+    void userWithOnlyCheckingShouldNotHaveBothAccounts() {
+        Account checkingAccount = new Account("2001", 1000.00);
+        User user = new User("Alex", "222222", "2222", checkingAccount, null);
+
+        assertFalse(user.hasBothAccounts());
+    }
+
+    @Test
+    void userWithOnlySavingsShouldNotHaveBothAccounts() {
+        Account savingsAccount = new Account("3001", 750.00);
+        User user = new User("Maria", "333333", "3333", null, savingsAccount);
+
+        assertFalse(user.hasBothAccounts());
     }
 }
